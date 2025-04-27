bc7f338394a5e3e64aaafae00943098a48193a00
Ridwan-z <rdw505n@gmail.com>
2025-04-28 06:31:51 +0700

    part 10

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -51,11 +51,55 @@ class TugasController extends Controller
         $tugas->tanggal_mulai = $request->tanggal_mulai;
         $tugas->tanggal_selesai = $request->tanggal_selesai;
         $tugas->save();
-        
+
         $user->is_tugas = true;
         $user->save();
 
 
         return redirect()->route('tugas')->with('success', 'Data Berhasil Ditambahkan');
+    }
+
+    public function edit($id)
+    {
+        $data = array(
+            "title" => "Edit Data Tugas",
+            "menuAdminTugas" => "active",
+            'tugas' => Tugas::with('user')->findOrFail($id),
+        );
+        return view('admin.tugas.edit', $data);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'tugas' => 'required',
+            'tanggal_mulai' => 'required',
+            'tanggal_selesai' => 'required'
+        ], [
+            'tugas.required' => 'Tugas Tidak Boleh Kosong',
+            'tanggal_mulai.required' => 'Tangga Mulai Tidak Boleh Kosong',
+            'tanggal_selesai.required' => 'Tanggal Selesai Tidak Boleh Kosong',
+        ]);
+
+
+        $tugas = Tugas::findOrFail($id);
+        $tugas->tugas = $request->tugas;
+        $tugas->tanggal_mulai = $request->tanggal_mulai;
+        $tugas->tanggal_selesai = $request->tanggal_selesai;
+        $tugas->save();
+
+        return redirect()->route('tugas')->with('success', 'Data Berhasil Di Edit');
+    }
+
+    public function destroy($id)
+    {
+        $tugas = Tugas::findOrFail($id);
+        $tugas->delete();
+
+        $user = User::where('id', $tugas->user_id)->first();
+        $user->is_tugas = false;
+        $user->save();
+
+        return redirect()->route('tugas')->with('success', 'Data Berhasil Di Hapus');
     }
 }
--- resources/views/admin/tugas/index.blade.php
+++ resources/views/admin/tugas/index.blade.php
@@ -17,7 +17,7 @@
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
                     <thead class="bg-primary text-white">
                         <tr class="text-center">
                             <th>No</th>
@@ -41,14 +41,18 @@
                                 </td>
 
                                 <td class="text-center">
-                                    <a href="{{ route('userEdit', $item->id) }}" class="btn btn-warning"><i
+                                    <button type="button" class="btn btn-info" data-toggle="modal"
+                                        data-target="#modalTugasShow{{ $item->id }}">
+                                        <i class="fas fa-eye"></i>
+                                    </button>
+                                    <a href="{{ route('tugasEdit', $item->id) }}" class="btn btn-warning"><i
                                             class="fas fa-edit"></i></a>
                                     <button type="button" class="btn btn-danger" data-toggle="modal"
-                                        data-target="#exampleModal{{ $item->id }}">
+                                        data-target="#modalTugasDestroy{{ $item->id }}">
                                         <i class="fas fa-trash"></i>
                                     </button>
 
-                                    @include('admin.user.modal')
+                                    @include('admin.tugas.modal')
                                 </td>
                             </tr>
                         @endforeach
